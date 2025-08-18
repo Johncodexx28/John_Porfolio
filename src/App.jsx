b39d40026effe21be06694c461e078377ecf0ceb
John Lloyd <cabanigdodong@gmail.com>
2025-08-18 21:15:59 +0800

    Fix import casing for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DarkVeil from "./components/DarkVeil";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import DarkVeil from "./components/Darkveil.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
 
 export default function App() {
   return (
